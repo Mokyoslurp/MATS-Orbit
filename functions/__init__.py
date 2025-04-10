e0b832740c9d71c7b59b802e3da993432b23b292
Mokyoslurp <mokyoslurp@gmail.com>
2025-04-10 11:29:54 +0200

    Move propagation to a separate function for more control

--- functions/__init__.py
+++ functions/__init__.py
@@ -4,6 +4,7 @@ from orekit.pyhelpers import setup_orekit_curdir
 vm = orekit.initVM()
 setup_orekit_curdir()
 
+from .propagate import propagate_all
 from .build_data_frame import build_data_frame
 from .constants import EARTH, INERTIAL_FRAME, ESRANGE_FRAME
 from .plots import plot_earth_2D, plot_earth_3D, plot_elevation
--- main.py
+++ main.py
@@ -1,15 +1,11 @@
 import orekit
 from orekit.pyhelpers import setup_orekit_curdir
-from org.orekit.utils import TimeStampedPVCoordinates
-
-from org.orekit.time import TimeScalesFactory, AbsoluteDate
-from org.orekit.propagation import Propagator
-from org.orekit.propagation.analytical.tle import TLE, TLEPropagator
-
+from org.orekit.propagation.analytical.tle import TLE
 
 from functions import (
     ESRANGE_FRAME,
     INERTIAL_FRAME,
+    propagate_all,
     build_data_frame,
     plot_elevation,
     plot_earth_2D,
@@ -25,29 +21,13 @@ with open("data/sat000054227.txt") as tle_file:
     tle_lines = tle_file.readlines()
 
 tles = [TLE(tle_lines[2 * i], tle_lines[2 * i + 1]) for i in range(len(tle_lines) // 2)]
-
-tle = tles[0]
+tles = tles[0:2]
 
 
 # Propagation
-# (Cast is necessary here because Java does not have auto type casting so we have to do it in python)
-propagator: Propagator = Propagator.cast_(TLEPropagator.selectExtrapolator(tle))
-
-extrapolated_date = AbsoluteDate(2002, 5, 7, 12, 0, 0.0, TimeScalesFactory.getUTC())
-final_date = extrapolated_date.shiftedBy(60.0 * 60 * 24)
-
-pv_vectors: list[TimeStampedPVCoordinates] = []
-
-while extrapolated_date.compareTo(final_date) <= 0.0:
-    # Get Position and velocity
-    pv = propagator.getPVCoordinates(extrapolated_date, INERTIAL_FRAME)
-    pv_vectors.append(pv)
-
-    # Increment date
-    extrapolated_date = extrapolated_date.shiftedBy(10.0)
+pv_vectors = propagate_all(tles)
 
 data_frame = build_data_frame(pv_vectors, INERTIAL_FRAME, ESRANGE_FRAME)
-
 
 # Plots
 plot_elevation(data_frame)
