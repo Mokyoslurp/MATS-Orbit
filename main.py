162652f720decff0aae5275f4625ed8f8065e695
Mokyoslurp <mokyoslurp@gmail.com>
2025-04-29 10:24:58 +0200

    Update example code with comparison between ISS and MATS orbits

--- main.py
+++ main.py
@@ -4,27 +4,50 @@ from functions import (
     load_tles,
     propagate_all,
     build_data_frame,
+    build_eclipse_data_frame,
     plot_elevation,
     plot_earth_2D,
     plot_earth_3D,
+    plot_local_orbit,
+    plot_global_orbit,
+    plot_sza,
 )
 
+from org.orekit.propagation.analytical.tle import TLE
 
-tles = load_tles("data/sat000054227.txt")
 
 # Set time interval: Year, Month, Day, Hour, Minute, Second (UTC)
-start_date = [2022, 11, 20, 0, 0, 0.0]
-end_date = [2022, 11, 26, 0, 0, 0.0]
+start_date = [2025, 4, 15, 0, 0, 0.0]
+end_date = [2025, 4, 16, 0, 0, 0.0]
 
-# start_date = [2025, 2, 20, 0, 0, 0.0]
-# end_date = [2025, 2, 26, 0, 0, 0.0]
+# MATS:
+mats_tles = load_tles("data/sat000054227.txt")
 
-# Propagation
-pv_vectors = propagate_all(tles, start_date, end_date)
+mats_pv_vectors, mats_events = propagate_all(mats_tles, start_date, end_date)
 
-data_frame = build_data_frame(pv_vectors, INERTIAL_FRAME, ESRANGE_FRAME)
+mats_data_frame = build_data_frame(mats_pv_vectors, INERTIAL_FRAME, ESRANGE_FRAME)
+mats_eclipse_data_frame = build_eclipse_data_frame(mats_events)
+
+
+# ISS:
+iss_tles = [
+    TLE(
+        "1 25544U 98067A   25105.53237150  .00014782  00000+0  27047-3 0  9993",
+        "2 25544  51.6375 257.3560 0005276  47.8113  31.7820 15.49569282505441",
+    )
+]
+
+iss_pv_vectors, iss_events = propagate_all(iss_tles, start_date, end_date, time_step=10.0)
+
+iss_data_frame = build_data_frame(iss_pv_vectors, INERTIAL_FRAME, ESRANGE_FRAME)
+iss_eclipse_data_frame = build_eclipse_data_frame(iss_events)
 
 # Plots
-plot_elevation(data_frame)
-plot_earth_3D(data_frame)
-plot_earth_2D(data_frame)
+# plot_elevation(mats_data_frame)
+# plot_earth_3D(mats_data_frame)
+# plot_earth_2D(mats_data_frame)
+# print(mats_eclipse_data_frame.head())
+
+plot_sza([mats_data_frame, iss_data_frame])
+plot_local_orbit([mats_data_frame, iss_data_frame])
+plot_global_orbit([mats_data_frame, iss_data_frame])
