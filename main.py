f6fd3786516158f3053aaf9c4dc180b4fd0c2c93
Mokyoslurp <mokyoslurp@gmail.com>
2025-05-08 00:32:06 +0200

    Update example code

--- main.py
+++ main.py
@@ -1,58 +1,50 @@
-from functions import (
-    ESRANGE_FRAME,
-    INERTIAL_FRAME,
-    load_tles,
-    propagate_all,
-    build_data_frame,
-    build_eclipse_data_frame,
-    build_orbit_parameters_data_frame,
-    plot_elevation,
+from src.util import load_tles, list_date_to_absolute_date  # noqa: E402
+from src.loop import loop_on_tles  # noqa: E402
+
+from src.eclipse import get_eclipse, plot_eclipse  # noqa: E402
+from src.orbital_parameters import get_orbital_parameters, plot_orbital_parameters  # noqa: E402
+from src.trajectory import (  # noqa: E402
+    get_trajectory,
     plot_earth_2D,
     plot_earth_3D,
-    plot_local_orbit,
+    plot_elevation,
     plot_global_orbit,
+    plot_local_orbit,
     plot_sza,
-    plot_apses,
 )
-
-from org.orekit.propagation.analytical.tle import TLE
+from src.dst import get_dsts, plot_dsts
 
 
 # Set time interval: Year, Month, Day, Hour, Minute, Second (UTC)
-start_date = [2025, 4, 15, 0, 0, 0.0]
-end_date = [2025, 4, 16, 0, 0, 0.0]
+start_date = list_date_to_absolute_date([2023, 1, 1, 0, 0, 0.0])
+end_date = list_date_to_absolute_date([2023, 1, 2, 0, 0, 0.0])
 
 # MATS:
-mats_tles = load_tles("data/sat000054227.txt")
+mats_tles = load_tles("data/sat000054227_fixed.txt")
 
-mats_pv_vectors, mats_events = propagate_all(mats_tles, start_date, end_date)
+mats_pv_vectors, mats_events = loop_on_tles(mats_tles, start_date, end_date, time_step=1 * 60.0)
 
-mats_data_frame = build_data_frame(mats_pv_vectors, INERTIAL_FRAME, ESRANGE_FRAME)
-mats_eclipse_data_frame = build_eclipse_data_frame(mats_events)
-mats_orbital_parameters_data_frame = build_orbit_parameters_data_frame(mats_tles)
+mats_trajectory = get_trajectory(mats_pv_vectors)
+mats_eclipse = get_eclipse(mats_events)
+mats_orbital_parameters = get_orbital_parameters(mats_tles)
 
 
-# ISS:
-iss_tles = [
-    TLE(
-        "1 25544U 98067A   25105.53237150  .00014782  00000+0  27047-3 0  9993",
-        "2 25544  51.6375 257.3560 0005276  47.8113  31.7820 15.49569282505441",
-    )
-]
-
-iss_pv_vectors, iss_events = propagate_all(iss_tles, start_date, end_date, time_step=10.0)
-
-iss_data_frame = build_data_frame(iss_pv_vectors, INERTIAL_FRAME, ESRANGE_FRAME)
-iss_eclipse_data_frame = build_eclipse_data_frame(iss_events)
+# DSTS
+dsts_files = ["dst01"]
+dsts = get_dsts(dsts_files)
 
 # Plots
-# plot_elevation(mats_data_frame)
-# plot_earth_3D(mats_data_frame)
-# plot_earth_2D(mats_data_frame)
-# print(mats_eclipse_data_frame.head())
 
-# plot_sza([mats_data_frame, iss_data_frame])
-# plot_local_orbit([mats_data_frame, iss_data_frame])
-# plot_global_orbit([mats_data_frame, iss_data_frame])
+plot_elevation(mats_trajectory)
+plot_earth_3D(mats_trajectory)
+plot_earth_2D(mats_trajectory)
+plot_eclipse(mats_eclipse)
 
-plot_apses(mats_orbital_parameters_data_frame)
+
+plot_sza(mats_trajectory)
+plot_local_orbit(mats_trajectory)
+plot_global_orbit(mats_trajectory)
+
+plot_orbital_parameters(mats_orbital_parameters)
+
+plot_dsts(dsts)
